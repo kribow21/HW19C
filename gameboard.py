5b8d334f03d2560ad166d3c77fbd795a8b645e82
kribow21 <kristininnotech@gmail.com>
2021-09-23 11:40:48 -0600

    made comments and check win function

--- gameboard.py
+++ gameboard.py
@@ -3,11 +3,11 @@ class GameBoard:
         self.winningRow = 0
         self.winningColumn = 2
         self.board = [
-            [" * ", " * ", "   ", " * ", " * ", " * "],
+            [" * ", " * ", "   ", " * ", " * ", " * "],     #ROW 0
             [
                 " * ",
                 "   ",
-                "   ",
+                "   ",      #ROW 1
                 "   ",
                 " * ",
                 " * ",
@@ -15,7 +15,7 @@ class GameBoard:
             [
                 " * ",
                 "   ",
-                " * ",
+                " * ",      #ROW 2
                 " * ",
                 "   ",
                 " * ",
@@ -23,29 +23,34 @@ class GameBoard:
             [
                 " * ",
                 "   ",
-                "   ",
+                "   ",      #ROW 3
                 "   ",
                 "   ",
                 " * ",
             ],
-            [" * ", " * ", " * ", " * ", " * ", " * "],
+            [" * ", " * ", " * ", " * ", " * ", " * "],     #ROW 4 
         ]
-
+# for lists in board container return the number of lists in the board container so i can iterate up to 5 so I = PLAYER ROW
     def printBoard(self, playerRow, playerColumn):
         for i in range(len(self.board)):
+# iterate through each item in the lists of each list in the container above so J = PLAYER COLUMN
             for j in range(len(self.board[i])):
                 if i == playerRow and j == playerColumn:
                     print("P", end="")
                 else:
                     print(self.board[i][j], end="")
             print("")
-
     def checkMove(self, testRow, testColumn):
         if self.board[testRow][testColumn].find("*") != -1:
             print("Can't move there!")
             return False
         return True
-
+    def checkWin(self, playerRow, playerColumn):
+        if self.board[self, playerRow == self.winningRow][playerColumn == self.winningColumn]:
+            print("You won!")
+            return True
+        else:
+            return False
     # TODO
     # Return True if the player is in the winning column and row
     # Return False otherwise
